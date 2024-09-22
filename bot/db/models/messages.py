c5db54397625bbcf7acc1db8c9aa71e7cbd5b1c9
aleksandr-ovchinnikov <ovchinnikoff.sandr@gmail.com>
2024-09-22 21:15:31 +0300

    fix

--- bot/db/models/messages.py
+++ bot/db/models/messages.py
@@ -10,9 +10,10 @@ class Message(Base):
     __tablename__ = 'messages'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(default=None, nullable=True)
+    user_id: Mapped[str] = mapped_column(default=None, nullable=True)
     username: Mapped[str] = mapped_column(default=None, nullable=True)
     request: Mapped[str] = mapped_column(default=None, nullable=True)
+    optional: Mapped[str] = mapped_column(default=None, nullable=True)
     request_type: Mapped[str] = mapped_column(default=None, nullable=True)
     response: Mapped[str] = mapped_column(default=None, nullable=True)
     response_type: Mapped[str] = mapped_column(default=None, nullable=True)
@@ -24,14 +25,16 @@ class Message(Base):
         user_id: int | None = None,
         username: str | None = None,
         request: str | None = None,
+        optional: str | None = None,
         request_type: str | None = None,
         response: str | None = None,
         response_type: str | None = None,
         ):
 
-        self.user_id = user_id
+        self.user_id = str(user_id)
         self.username = username
         self.request = request
+        self.optional = optional
         self.request_type = request_type
         self.response = response
         self.response_type = response_type
